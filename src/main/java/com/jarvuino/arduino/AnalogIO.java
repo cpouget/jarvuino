42920cf02bbb4c345aff657211cbe415710c919a
cyrille <cyrille.pouget@gmail.com>
2015-12-04 14:29:58 +0100

    AnalogIO

--- src/main/java/com/jarvuino/arduino/AnalogIO.java
+++ src/main/java/com/jarvuino/arduino/AnalogIO.java
@@ -1,34 +1,66 @@
 package com.jarvuino.arduino;
 
 import com.jarvuino.arduino.constants.ReferenceVoltage;
+import com.jarvuino.core.ArduinoChannelWrapper;
+import com.jarvuino.core.io.ResponseFuture;
+import com.jarvuino.core.io.handler.SynchronousResponseChannelHandler;
 import io.netty.channel.Channel;
 import org.apache.commons.lang3.NotImplementedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static java.lang.String.format;
 
 public class AnalogIO {
 
     private static final Logger LOG = LoggerFactory.getLogger(AnalogIO.class);
 
-    private Channel channel;
+    private ArduinoChannelWrapper channel;
 
-    public AnalogIO(Channel channel) {
+    public AnalogIO(ArduinoChannelWrapper channel) {
         this.channel = channel;
     }
 
-    public void analogReference(ReferenceVoltage referenceVoltage) {
-        throw new NotImplementedException("AnalogIO.analogReference is not implemented");
+    public void analogReference(ReferenceVoltage refVolt) throws ArduinoIOException {
+        LOG.debug("send command: ref-volt/{}", refVolt.ordinal());
+
+        try {
+            channel.get().writeAndFlush(format(":ref-volt/%d\n", refVolt.ordinal()));
+        } catch (Exception e) {
+            throw new ArduinoIOException(e);
+        }
     }
 
-    public void analogWrite(int pin, int value) {
+    public void analogWrite(int pin, int value) throws ArduinoIOException {
         checkArgument(value >= 0 && value <= 255, "duty cycle must be between 0 (always off) and 255 (always on)");
 
-        throw new NotImplementedException("AnalogIO.analogWrite is not implemented");
+        LOG.debug("send command: a-write/{}/{}", pin, value);
+
+        try {
+            channel.get().writeAndFlush(format(":a-write/%d/%d\n", pin, value)).get();
+        } catch (Exception e) {
+            throw new ArduinoIOException(e);
+        }
     }
 
-    public int analogRead(int pin) {
-        throw new NotImplementedException("AnalogIO.analogRead is not implemented");
+    public String analogRead(int pin) throws ArduinoIOException {
+
+        LOG.debug("send command: a-read/{}", pin);
+
+        ResponseFuture responseFuture = new ResponseFuture(channel.synchronousHandler);
+
+        String msg = null;
+        try {
+            channel.get().writeAndFlush(format(":a-read/%d\n", pin)).get();
+            msg = responseFuture.get();
+        } catch (Exception e) {
+            throw new ArduinoIOException(e);
+        }
+
+        LOG.debug("read value: {}", msg);
+
+        return msg;
+
     }
 }
